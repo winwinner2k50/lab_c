b06a0f13e8e7a2dd8698aa27535239afa968be19
Eremin Georgii <eremings@student.bmstu.ru>
2024-11-04 17:16:11 +0300

    fix dell_col

--- lab_08/matirx_operation.c
+++ lab_08/matirx_operation.c
@@ -66,7 +66,8 @@ void matrix_dell_col(void ***lins, size_t *n, size_t *m, size_t type_size, size_
         }
     }
     (*m)--;
-    (*lins)[pos_y] = realloc((*lins)[pos_y], (*m) * sizeof(char*));
+    for (size_t i = 0; i < (*n); i++)
+        (*lins)[i] = realloc((*lins)[i], (*m) * sizeof(char*));
 }
 
 int matrix_find_el(void **lins, size_t n, size_t m, size_t type_size, void **el_found, size_t *x, size_t *y, int (comparator)(void*, void*))
@@ -88,8 +89,6 @@ int matrix_find_el(void **lins, size_t n, size_t m, size_t type_size, void **el_
         }
     return 0;
 }
-
-
 
 int matrix_dell_something_with_el(void ***lins, size_t *n, size_t *m, size_t type_size,
 int (comparator)(void*, void*), 
@@ -114,6 +113,13 @@ int matrix_square_with_dell(void ***lins, size_t *n, size_t *m, size_t type_size
     }
     return 0;
 }
+
+// int matrix_add_str(void ***lins, size_t *n, size_t *m, size_t type_size, size_t pos_x, size_t pos_y)
+// {
+//     (*n)++;
+//     (*lins) = realloc(*lins, (*n) * sizeof(char*));
+
+// }
 
 int matrix_clear(void ***lins, size_t n)
 {
